4e420c04f47b7be03ddcd61350ab8e4960cb468f
Naval jangir <91688980+Naval22@users.noreply.github.com>
2024-05-03 23:38:01 +0530

    Update application.py

--- application.py
+++ application.py
@@ -5,11 +5,11 @@ application = Flask(__name__)
 
 app=application
 
-@app.route('/')
-def home_page():
-    return render_template('index.html')
+# @app.route('/')
+# def home_page():
+#     return render_template('index.html')
 
-@app.route('/predict',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 
 def predict_datapoint():
     if request.method=='GET':
